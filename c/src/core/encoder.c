31eeeeafb9e7027e4c9d5b53b4be0139df6c6c27
Justin Ross <jross@apache.org>
2021-02-07 08:20:58 -0500

    WIP

--- c/src/core/encoder.c
+++ c/src/core/encoder.c
@@ -84,7 +84,8 @@ static inline uint8_t pn_type2code(pn_encoder_t *encoder, pn_type_t type)
   }
 }
 
-static uint8_t pn_node2code(pn_encoder_t *encoder, pni_node_t *node)
+__attribute__((always_inline))
+static inline uint8_t pn_node2code(pn_encoder_t *encoder, pni_node_t *node)
 {
   switch (node->atom.type) {
   case PN_LONG:
