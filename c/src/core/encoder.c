90a135a25447e91aec84508371f733a6bfe63b2e
Justin Ross <jross@apache.org>
2021-03-04 11:15:10 -0500

    WIP

--- c/src/core/encoder.c
+++ c/src/core/encoder.c
@@ -385,7 +385,7 @@ static inline int pni_encoder_encode_compound8(pn_encoder_t *encoder, pn_data_t 
   char *pos = encoder->position;
   encoder->position = start;
 
-  pn_encoder_writef8(encoder, (size_t) (pos - start - 1)); // XXX Need the cast?
+  pn_encoder_writef8(encoder, pos - start - 1);
 
   encoder->position = pos;
 
@@ -420,7 +420,7 @@ static int pni_encoder_encode_compound32(pn_encoder_t *encoder, pn_data_t *data)
   char *pos = encoder->position;
   encoder->position = start;
 
-  pn_encoder_writef32(encoder, (size_t) (pos - start - 4));
+  pn_encoder_writef32(encoder, pos - start - 4);
 
   encoder->position = pos;
 
